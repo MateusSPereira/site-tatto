4385ff856f6e15390a8dab2e8b915ccb5f0c4425
MateusSPereira <mateusfelipsp@gmail.com>
2023-11-01 13:31:16 +0000

    adiçao calendar

---
+++ script.js
@@ -0,0 +1,62 @@
+function agd1(){
+    var janeiro = document.getElementById('janeiro')
+    janeiro.innerHTML = '1 2 3 4 5 6 7 8 9 10 <br>'
+    janeiro.innerHTML += '11 12 13 14 15 16 17 <br> 18 19 20 21 22 23 24<br> 25 26 27 28 29 30 <br>'
+}
+function agd2(){
+    var fevereiro = document.getElementById('fevereiro')
+    fevereiro.innerHTML = '1 2 3 4 5 6 7 8 9 10 <br>'
+    fevereiro.innerHTML += '11 12 13 14 15 16 17 <br> 18 19 20 21 22 23 24<br> 25 26 27 28 29 30 <br>'
+}
+function agd3(){
+    var marco = document.getElementById('marco')
+    marco.innerHTML = '1 2 3 4 5 6 7 8 9 10 <br>'
+    marco.innerHTML += '11 12 13 14 15 16 17 <br> 18 19 20 21 22 23 24<br> 25 26 27 28 29 30 <br>'
+}
+function agd4(){
+    var abril = document.getElementById('abril')
+    abril.innerHTML = '1 2 3 4 5 6 7 8 9 10 <br>'
+    abril.innerHTML += '11 12 13 14 15 16 17 <br> 18 19 20 21 22 23 24<br> 25 26 27 28 29 30 <br>'
+}
+function agd5(){
+    var maio = document.getElementById('maio')
+    maio.innerHTML = '1 2 3 4 5 6 7 8 9 10 <br>'
+    maio.innerHTML += '11 12 13 14 15 16 17 <br> 18 19 20 21 22 23 24<br> 25 26 27 28 29 30 <br>'
+}
+function agd6(){
+    var junho = document.getElementById('junho')
+    junho.innerHTML = '1 2 3 4 5 6 7 8 9 10 <br>'
+    junho.innerHTML += '11 12 13 14 15 16 17 <br> 18 19 20 21 22 23 24<br> 25 26 27 28 29 30 <br>'
+}
+function agd7(){
+    var julho = document.getElementById('julho')
+    julho.innerHTML = '1 2 3 4 5 6 7 8 9 10 <br>'
+    julho.innerHTML += '11 12 13 14 15 16 17 <br> 18 19 20 21 22 23 24<br> 25 26 27 28 29 30 <br>'
+}
+function agd8(){
+    var  agosto= document.getElementById('agosto')
+    agosto.innerHTML = '1 2 3 4 5 6 7 8 9 10 <br>'
+    agosto.innerHTML += '11 12 13 14 15 16 17 <br> 18 19 20 21 22 23 24<br> 25 26 27 28 29 30 <br>'
+}
+function agd9(){
+    var setembro = document.getElementById('setembro')
+    setembro.innerHTML = '1 2 3 4 5 6 7 8 9 10 <br>'
+    setembro.innerHTML += '11 12 13 14 15 16 17 <br> 18 19 20 21 22 23 24<br> 25 26 27 28 29 30 <br>'
+}
+function agd10(){
+    var outubro = document.getElementById('outubro')
+    outubro.innerHTML = '1 2 3 4 5 6 7 8 9 10 <br>'
+    outubro.innerHTML += '11 12 13 14 15 16 17 <br> 18 19 20 21 22 23 24<br> 25 26 27 28 29 30 <br>'
+}
+function agd11(){
+    var novembro = document.getElementById('novembro')
+    novembro.innerHTML = '1 2 3 4 5 6 7 8 9 10 <br>'
+    novembro.innerHTML += '11 12 13 14 15 16 17 <br> 18 19 20 21 22 23 24<br> 25 26 27 28 29 30 <br>'
+}
+function agd12(){
+    var dezembro = document.getElementById('dezembro')
+    dezembro.innerHTML = '1 2 3 4 5 6 7 8 9 10 <br>'
+    dezembro.innerHTML += '11 12 13 14 15 16 17 <br> 18 19 20 21 22 23 24<br> 25 26 27 28 29 30 <br>'
+}
+
+
